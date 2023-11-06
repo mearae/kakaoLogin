f25d9e2fe91ff62f1ee0b52ae03f8009a72a3546
mearae <>
2023-11-06 16:01:13 +0900

    user와 kakao 회원가입 및 로그아웃

--- kakaoLogin/src/main/java/com/example/demo/kakao/KakaoController.java
+++ kakaoLogin/src/main/java/com/example/demo/kakao/KakaoController.java
@@ -1,14 +1,12 @@
 package com.example.demo.kakao;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,7 +16,7 @@ public class KakaoController {
 
     // 인가코드 받기 : https://kauth.kakao.com/oauth/authorize 는
     // kakao Developer에 보면 Get으로 연결하라고 함
-    @GetMapping(value = "/kakao/oauth")
+    @GetMapping("/kakao/oauth")
     public String kakaoConnect(Error error, HttpStatus status) {
         // "redirect:" -> 뒤에 오는 http 링크(String)로 이동
         // @RestController 일 경우에는 링크 이동이 불가(객체만 return 가능함)
@@ -29,7 +27,7 @@ public class KakaoController {
         return "redirect:" + link;
     }
 
-    @GetMapping(value = "/kakao/relogin")
+    @GetMapping("/kakao/relogin")
     public String kakaoAutoConnect(Error error){
         String link = kakaoService.kakaoAutoConnect();
 
@@ -45,42 +43,43 @@ public class KakaoController {
         return "redirect:" + link;
     }
 
-    @GetMapping(value = "/kakao/join")
+    @GetMapping("/kakao/join")
     public String kakaoJoin(HttpServletRequest req){
         kakaoService.kakaoJoin(req.getSession());
 
         return "redirect:/logined.html";
     }
 
-    @GetMapping(value = "/kakao/logout")
+    @GetMapping("/kakao/logout")
     public String kakaoLogout(HttpServletRequest req){
         kakaoService.kakaoLogout(req.getSession());
 
         return "redirect:/index.html";
     }
 
-    @GetMapping(value = "/kakao/fulllogout")
+    @GetMapping("/kakao/fulllogout")
     public String kakaoFullLogout(){
         String link = kakaoService.kakaoFullLogout();
 
         return "redirect:" + link;
     }
 
-    @GetMapping(value = "/kakao/disconnect")
+    @GetMapping("/kakao/disconnect")
     public String kakaoDisconnect(HttpServletRequest req){
         kakaoService.kakaoDisconnect(req.getSession());
 
-        return "redirect:/loginByKakao.html";
+        //return ResponseEntity.ok(ApiUtils.success(null));
+        return "redirect:/index.html";
     }
 
-    @GetMapping(value = "/kakao/userlist")
+    @GetMapping("/kakao/userlist")
     public String kakaoDisconnect(){
         kakaoService.kakaoUserList();
 
         return "redirect:/logined.html";
     }
 
-    @GetMapping(value = "/kakao/end")
+    @GetMapping("/kakao/end")
     public void endServer(){
         kakaoService.endServer();
     }
