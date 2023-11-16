772d918f819e38db64fc92711208ab2eb15dbc81
mearae <qownsbjh@gmail.com>
2023-11-16 17:32:25 +0900

    최종 수정
    로그인 했을 때만 토큰 부여하고 사이트 접근 가능, 토큰 갱신은 없음

--- kakaoLogin/src/main/java/com/example/demo/user/UserController.java
+++ kakaoLogin/src/main/java/com/example/demo/user/UserController.java
@@ -3,7 +3,6 @@ package com.example.demo.user;
 import com.example.demo.core.security.CustomUserDetails;
 import com.example.demo.core.security.JwtTokenProvider;
 import com.example.demo.core.utils.ApiUtils;
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,29 +19,31 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user/join")
-    public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDto joinDto, Error error){
+    public ResponseEntity<Object> join(@RequestBody @Valid UserRequest.JoinDto joinDto, Error error){
         userService.join(joinDto);
 
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @PostMapping("/user/check")
-    public ResponseEntity<?> check(@RequestBody @Valid UserRequest.JoinDto joinDto, Error error){
+    public ResponseEntity<Object> check(@RequestBody @Valid UserRequest.JoinDto joinDto, Error error){
         userService.checkEmail(joinDto.getEmail());
 
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @PostMapping("/user/oauth")
-    public ResponseEntity<?> connect(@RequestBody @Valid UserRequest.JoinDto joinDto, Error error){
+    public ResponseEntity<Object> connect(@RequestBody @Valid UserRequest.JoinDto joinDto, Error error){
         String jwt = userService.connect(joinDto);
 
+        User user = new User();
+
         return ResponseEntity.ok().header(JwtTokenProvider.HEADER, jwt)
-                .body(ApiUtils.success(null));
+                .body(ApiUtils.success(user));
     }
 
     @PostMapping("/user/login")
-    public ResponseEntity<?> login(@RequestBody @Valid UserRequest.JoinDto joinDto, HttpServletRequest req, Error error){
+    public ResponseEntity<Object> login(@RequestBody @Valid UserRequest.JoinDto joinDto, HttpServletRequest req, Error error){
         userService.login(joinDto, req.getSession());
         return ResponseEntity.ok(ApiUtils.success(null));
     }
@@ -53,20 +54,20 @@ public class UserController {
     }
 
     @GetMapping("/user/users")
-    public ResponseEntity<?> printUsers(){
+    public ResponseEntity<Object> printUsers(){
         userService.findAll();
 
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @GetMapping("/user/accessed")
-    public JsonNode isAccessed(HttpServletRequest req){
-        JsonNode ll = userService.isAccessed(req.getSession());
+    public String isAccessed(HttpServletRequest req){
+        String ll = userService.isAccessed(req.getSession());
         return ll;
     }
 
     @PostMapping("/user/user_info")
-    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+    public ResponseEntity<ApiUtils.ApiResult<User>> getCurrentUser(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         if (customUserDetails.getUser() == null){
             return ResponseEntity.ok(ApiUtils.error("현재 로그인된 user가 없습니다.", HttpStatus.UNAUTHORIZED));
         }
